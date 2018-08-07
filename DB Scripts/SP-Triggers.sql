d44d0ef93d04dfbd7d6fd60da75735bffd2a7f2f
Unknown <car.mor0612@gmail.com>
2018-08-07 09:40:15 -0600

    Triggers Factura y SP faltantes

--- DB Scripts/SP-Triggers.sql
+++ DB Scripts/SP-Triggers.sql
@@ -88,7 +88,7 @@ begin
 		end
 end
 
-create trigger TRG_stock1 on FacturaDesg --trigger para ir eliminando los productos del stock segun se van vendiendo
+create trigger TRG_stock1 on FacturaDesg --trigger para ir eliminando o agregando productos los productos del stock segun se van vendiendo
 after insert, update
 as
 begin
@@ -115,4 +115,53 @@ begin
 		update Producto
 		set Stock = (@oldstock-@newcant)
 		where IdProducto = (select idproducto from inserted);
+end
+
+create procedure add_permisos (@Descripcion nvarchar(50))
+as
+begin try
+	insert into Permisos(descripcion)
+	values (@Descripcion)
+end try
+begin catch
+	IF @@ERROR = 2627
+	-- Revisa si el permiso ya existe y devuelve un campo en una tabla 'dual'   
+	Select 'El Permiso ya existe'
+end catch
+
+create procedure add_usuario (@username nvarchar(50), @password nvarchar(50), @nombre nvarchar(50),@apellido nvarchar(50))
+as
+begin try
+	insert into Usuario(username, password,nombre,apellido)
+	values(@username,@password,@nombre,@apellido)
+end try
+begin catch
+	IF @@ERROR = 2627
+	-- Revisa si el usuario ya existe y devuelve un campo en una tabla 'dual'   
+	Select 'El usuario ya existe'
+end catch
+
+create procedure add_permisos_usuario(@username nvarchar(50),@idpermiso nvarchar(50),@accesoPermiso bit)
+as
+begin
+	
+	if exists(select * from permisosxusuario where username = @username and idpermiso = @idpermiso)
+		begin
+			select 'El usuario ya tiene el permiso ingresado'
+		end
+	else
+		begin
+			insert into permisosxususario(username,accesopermiso,idpermiso)
+			values(@username,@accesoPermiso, @idpermiso)
+		end
+end
+
+create trigger TRG_fact_total on FacturaDesg --trigger para actualizar el total de la factura
+after insert, update
+as
+begin
+	declare @total float
+	declare @factura int
+	select @factura = numfac from inserted
+	select @total = sum(precio*cantidad) from facturadesg where numfac=@factura
 end
